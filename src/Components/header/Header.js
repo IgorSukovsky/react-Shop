fc09b8ae91249ae71275a1787df8e0a76174c32b
IgorSukovsky <Sukovsky70@gmail.com>
2021-06-07 18:17:30 +0300

    add commit

---
+++ src/Components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderList from "./headerlist/HeaderList";
+
+const Header = () => {
+  return (
+    <header>
+      <HeaderList />
+    </header>
+  );
+};
+
+export default Header;
